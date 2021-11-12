cdb884cfe24c22d0a98f844753710ba4cd8e498a
Jee Kim CW <ac38pzz@mmm.com>
2021-11-12 09:11:21 -0800

    Created a simple webpage with two of my sketches.

--- dvd_bouncer.js
+++ dvd_bouncer.js
@@ -1,70 +1,91 @@
-class Rectangle {
-  constructor(x, y, height, width) 
-  {
-    this.x = x;
-    this.y = y;
-    this.height = height;
-    this.width = width;
-  }
-  draw()
-  {
-    line(this.x, this.y, this.x+this.width, this.y);
-    line(this.x+this.width, this.y, this.x+this.width, this.y+this.height);
-    line(this.x+this.width, this.y+this.height, this.x, this.y+this.height);
-    line(this.x, this.y+this.height, this.x, this.y);
-  }
-}
 
-class VelocityRectangle extends Rectangle {
-  constructor(x, y, height, width, speedX, speedY)
-  {
-    super(x, y, height, width);
-    this.speedX = speedX;
-    this.speedY = speedY;
+var s = function (p) {
+  class Rectangle {
+    constructor(x, y, height, width) 
+    {
+      this.x = x;
+      this.y = y;
+      this.height = height;
+      this.width = width;
+    }
+    draw()
+    {
+      p.line(this.x, this.y, this.x+this.width, this.y);
+      p.line(this.x+this.width, this.y, this.x+this.width, this.y+this.height);
+      p.line(this.x+this.width, this.y+this.height, this.x, this.y+this.height);
+      p.line(this.x, this.y+this.height, this.x, this.y);
+    }
   }
   
-  animate()
-  {
-    this.draw();
-    this.check_border()
-    this.x += this.speedX;
-    this.y += this.speedY;
-  }
-  
-  check_border()
-  {
-    if (this.x <= 0) {
-      this.speedX *= -1;
+  class VelocityRectangle extends Rectangle {
+    constructor(x, y, height, width, speedX, speedY)
+    {
+      super(x, y, height, width);
+      this.speedX = speedX;
+      this.speedY = speedY;
     }
     
-    if (this.y <= 0) {
-      this.speedY *= -1;
+    animate()
+    {
+      this.draw();
+      this.check_border()
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+    
+    check_border()
+    {
+      if (this.x <= 0) {
+        this.speedX *= -1;
+      }
+      
+      if (this.y <= 0) {
+        this.speedY *= -1;
+      }    
+      
+      if (this.x+this.width >= CANVAS_SIZE_X){
+        this.speedX *= -1;
+      }
+      
+      if (this.y+this.height >= CANVAS_SIZE_Y) {
+        this.speedY *= -1;
+      }
     }    
-    
-    if (this.x+this.width >= CANVAS_SIZE_X){
-      this.speedX *= -1;
-    }
-    
-    if (this.y+this.height >= CANVAS_SIZE_Y) {
-      this.speedY *= -1;
-    }
   }
-}
+
+  var CANVAS_SIZE_X = 400;
+  var CANVAS_SIZE_Y = 400;
+
+  p.setup = function() {
+    p.createCanvas(CANVAS_SIZE_X, CANVAS_SIZE_Y);
+    myRectangle = new Rectangle(100, 100, 100, 100);
+    AnimatedRect = new VelocityRectangle(100, 50, 50, 50, 1, 1);
+  };
+
+  p.draw = function() {
+    p.background(220);
+    AnimatedRect.animate();
+    // myRectangle.draw();      
+  };
+
+};
+
+var myp5 = new p5(s, 'c1');
+
+// var s = function( p ) { // p could be any variable name
+//   var x = 100; 
+//   var y = 100;
+//   p.setup = function() {
+//     p.createCanvas(400, 200);
+//   };
+
+//   p.draw = function() {
+//     p.background(0);
+//     p.fill(255);
+//     p.rect(x,y,50,50);
+//   };
+// };
+// var myp5 = new p5(s, 'c1');
 
 
-  CANVAS_SIZE_X = 400;
-  CANVAS_SIZE_Y = 400;
 
-function setup() {
-  createCanvas(CANVAS_SIZE_X, CANVAS_SIZE_Y);
-
-  myRectangle = new Rectangle(100, 100, 100, 100);
-  AnimatedRect = new VelocityRectangle(100, 50, 50, 50, 1, 1);
-}
-
-function draw() {
-  background(220);
-  AnimatedRect.animate();
-  // myRectangle.draw();
-  
-}
--- rotating_motion.js
+++ rotating_motion.js
@@ -1,50 +1,75 @@
-function setup() {
-  frameRate(30);
-  createCanvas(400, 400);
-  background(100);
-}
-
-class Animated_Object{
+var t = function (p) {
+  class Animated_Object{
   
-  constructor(_x, _y, _radius, _velocity)
-  {
-    this.x = _x;
-    this.y = _y;
-    this.centerX = _x;
-    this.centerY = _y;
-    this.r = _radius
-    this.velocity = _velocity;
-    this.deg = 0;
+    constructor(_x, _y, _radius, _velocity)
+    {
+      this.x = _x;
+      this.y = _y;
+      this.centerX = _x;
+      this.centerY = _y;
+      this.r = _radius
+      this.velocity = _velocity;
+      this.deg = 0;
+    }
+    
+    
+    tick()
+    {
+      p.circle(this.x, this.y, this.r)
+      
+      this.increment()
+      p.angleMode(p.DEGREES);
+      this.x = this.centerX + this.r*p.cos(this.deg);
+      this.y = this.centerY + this.r*p.sin(this.deg);
+      
+      // console.log(this.x);
+      // console.log(this.y);
+      // console.log(this.deg);
+      
+    }
+    
+    increment()
+    {
+      this.deg = (this.deg + 10) % 360;
+      console.log(this.deg);
+    }
   }
   
+  var MyCircle = new Animated_Object(200, 200, 10, 1);
+  var MyCircle_1 = new Animated_Object(210, 210, 10, 1);
   
-  tick()
-  {
-    circle(this.x, this.y, this.r)
-    
-    this.increment()
-    angleMode(DEGREES);
-    this.x = this.centerX + this.r*cos(this.deg);
-    this.y = this.centerY + this.r*sin(this.deg);
-    
-    // console.log(this.x);
-    // console.log(this.y);
-    // console.log(this.deg);
-    
-  }
+  p.setup = function() {
+    p.frameRate(30);
+    p.createCanvas(400, 400);
+    p.background(100);
+  };
   
-  increment()
-  {
-    this.deg = (this.deg + 10) % 360;
-    // console.log(this.deg);
-  }
+  p.draw = function() {
+    p.background(100);
+    MyCircle.tick();
+    MyCircle_1.tick();
+  };
 }
 
-let MyCircle = new Animated_Object(200, 200, 10, 1);
-let MyCircle_1 = new Animated_Object(210, 210, 10, 1);
+var myp5 = new p5(t, 'c2');
 
-function draw() {
-  background(100);
-  MyCircle.tick();
-  MyCircle_1.tick();
-}
+// var t = function( p ) { 
+//   var x = 100.0; 
+//   var y = 100; 
+//   var speed = 2.5; 
+//   p.setup = function() {
+//     p.createCanvas(400, 200);
+//   };
+
+//   p.draw = function() {
+//     p.background(100);
+//     p.fill(1);
+//     x += speed; 
+//     if(x > p.width){
+//       x = 0; 
+//     }
+//     p.ellipse(x,y,50,50);
+
+//   };
+// };
+// var myp5 = new p5(t, 'c2');
